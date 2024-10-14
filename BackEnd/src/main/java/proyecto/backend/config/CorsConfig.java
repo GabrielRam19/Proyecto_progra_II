05bb6eea28d0b0dbcd1cee28b85b21ec7f91ddb3
Juan Gabriel Ramirez Gramajo <jramirez@GRUPOTIR.COM>
2024-10-14 17:17:48 -0600

    Se cambia configuracion de cors

--- BackEnd/src/main/java/proyecto/backend/config/CorsConfig.java
+++ BackEnd/src/main/java/proyecto/backend/config/CorsConfig.java
@@ -1,23 +1,18 @@
 package proyecto.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedMethod("*");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas
+                .allowedOrigins("http://localhost:3000") // Cambia esto por el dominio de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite el uso de cookies si es necesario
     }
 }
